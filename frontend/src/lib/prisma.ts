c083cc65c74a7230228cd72a90149eb9cd7bc854
Harjot Singh Rana <HarjjotSinghh@gmail.com>
2025-07-27 09:15:33 +0530

    fix: deployment issues

--- frontend/src/lib/prisma.ts
+++ frontend/src/lib/prisma.ts
@@ -1,8 +1,7 @@
 import { PrismaClient } from "@prisma/client/edge";
-import { withAccelerate } from '@prisma/extension-accelerate'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate())
+  return new PrismaClient() as PrismaClient;
 };
 
 declare global {
